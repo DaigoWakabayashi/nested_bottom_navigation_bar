12d7735e4d98b899d95ebc82a5c86276ce870f2a
DaigoWakabayashi <mamushi.journey@gmail.com>
2022-07-14 22:11:47 +0900

    第2階層のページを作成
    # ==================== Emojis ====================
    # 🎉  :tada: 初めてのコミット（Initial Commit）
    # 🔖  :bookmark: バージョンタグ（Version Tag）
    # ✨  :sparkles: 新機能（New Feature）
    # 🐛  :bug: バグ修正（Bagfix）
    # ♻️  :recycle: リファクタリング(Refactoring)
    # 📚  :books: ドキュメント（Documentation）
    # 🎨  :art: デザインUI/UX(Accessibility)
    # 🐎  :horse: パフォーマンス（Performance）
    # 🔧  :wrench: ツール（Tooling）
    # 🚨  :rotating_light: テスト（Tests）
    # 💩  :hankey: 非推奨追加（Deprecation）
    # 🗑️  :wastebasket: 削除（Removal）
    # 🚧  :construction: WIP(Work In Progress)
    
    # ==================== Format ====================
    # :emoji: Subject
    #
    # Commit body...

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:nested_bottom_navigation_bar/pages/detail_page.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Settings')),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Settings'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push<void>(
+                  context,
+                  MaterialPageRoute<void>(
+                    builder: (BuildContext context) => const DetailPage(),
+                  ),
+                );
+              },
+              child: const Text('詳細ページへ'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/pages/timeline_page.dart
+++ lib/pages/timeline_page.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:nested_bottom_navigation_bar/pages/detail_page.dart';
 
 class TimelinePage extends StatelessWidget {
   const TimelinePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Timeline')),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Timeline'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push<void>(
+                  context,
+                  MaterialPageRoute<void>(
+                    builder: (BuildContext context) => const DetailPage(),
+                  ),
+                );
+              },
+              child: const Text('詳細ページへ'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
